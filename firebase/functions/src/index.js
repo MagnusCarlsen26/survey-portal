8d6cb798f34a1f6189766bb21f4e3632287b19b5
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-08-20 19:41:28 +0530

    Created users db and isAccess function

--- firebase/functions/src/index.js
+++ firebase/functions/src/index.js
@@ -1,23 +1,65 @@
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import functions from 'firebase-functions'
+import admin from 'firebase-admin'
 import {
     beforeUserCreated,
     beforeUserSignedIn,
 } from "firebase-functions/v2/identity";
   
+admin.initializeApp();
+const db = admin.firestore();
+
 export const beforecreated = beforeUserCreated((event) => {
     const user = event.data;
-
+ 
     if (!user?.email?.includes('@iitj.ac.in')) {
-      throw new HttpsError('invalid-argument', "Unauthorized email");
+      throw new functions.https.HttpsError('Please signup with IITJ email', "Unauthorized email");
     }
 });
 
-// export const beforesignedin = beforeUserSignedIn((event) => {
-//     // TODO
-// });
+export const onUserSignup = functions.auth.user().onCreate(async(user) => {
+  const email = user.email;
+  const uuid = user.uid;
+  const name = user.displayName
+ 
+  console.log(`New user signed up with email: ${email} and UID: ${uuid}`);
+  let docRef = db.collection('users').doc(user.uid);
+  await docRef.set({
+    email,
+    uuid,
+    name,
+    cat: admin.firestore.FieldValue.serverTimestamp(),
+    surveyStatus : false,
+    isAccess : false
+  });
+  return 0
+});
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const beforesignedin = beforeUserSignedIn((event) => {
+    const user = event.data;
+ 
+    if (!user?.email?.includes('@iitj.ac.in')) {
+      throw new functions.https.HttpsError('Please signin with IITJ email', "Unauthorized email");
+    }
+});
+
+export const isAccess = onRequest(async(req,res) => {
+  const uuid = req.query.uuid
+  try {
+    const docRef = db.collection('users').doc(uuid);
+    const docSnap = await docRef.get();
+    res.send(docSnap.data())
+    // if (docSnap.exists) {
+    //   res.status(200).send("Document data:", docSnap.data());
+    // } else {
+    //   res.status(404).send("No such document!");
+    // }
+  } catch (error) {
+    res.status(500).send(error,uuid)
+  }
+}) 
+
+export const helloWorld = onRequest((req, res) => {
+
+    res.send("Hello from Firebase!");
 });
