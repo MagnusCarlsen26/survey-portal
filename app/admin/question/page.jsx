9caec71f771983f805a7e3bb704c8ae721ae96a1
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-08-22 23:17:11 +0530

    some UI for admin/question. Good day GG

--- app/admin/question/page.jsx
+++ app/admin/question/page.jsx
@@ -1,84 +1,56 @@
 "use client"
 import { db,functions } from '@/firebase/confing'
-import { getFirestore, collection, addDoc,setDoc,doc } from 'firebase/firestore';
 import { httpsCallable } from 'firebase/functions';
+import { useState } from 'react';
 
-const doctors = [
-    {
-        name: 'Dr. Mukul Kumar',
-        specialty: 'General Physician',
-        experience: 9,
-        consultationFees: '1000',
-        distance: 4,
-        rating: '95%',
-        ratingsCount: 189,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2Fdownload_image_1717249519334.png?alt=media&amp;token=994bf234-6d7a-4506-b700-c11e0bf22278'
-    },
-    {
-        name: 'Dr. M Gowri',
-        specialty: 'General Physician',
-        experience: 9,
-        consultationFees: '900',
-        distance: 4,
-        rating: '100%',
-        ratingsCount: 186,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2FScreenshot%202024-06-13%20005519.jpg?alt=media&amp;token=90dee180-5457-4668-b41c-e2e753cb8a1b'
-    },
-    {
-        name: 'Dr. Chaitra Nayak',
-        specialty: 'General Physician',
-        experience: 10,
-        consultationFees: '1500',
-        distance: 3.5,
-        rating: '100%',
-        ratingsCount: 480,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2Ffemale%20old.jpg?alt=media&amp;token=4550d7f4-0f3e-4009-98a3-26629dc3af94'
-    },
-    {
-        name: 'Dr. Animesh Gupta',
-        specialty: 'General Physician',
-        experience: 15,
-        consultationFees: '1200',
-        distance: 0.2,
-        rating: '95%',
-        ratingsCount: 102,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2Fdownload_image_1717312280645.png?alt=media&amp;token=786673ea-2f7f-4608-be1b-65f46e83da7f'
-    },
-    {
-        name: 'Dr. Rajendra Shankar',
-        specialty: 'General Physician',
-        experience: 18,
-        consultationFees: '2000',
-        distance: 3,
-        rating: '97%',
-        ratingsCount: 502,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2Fmale%20old.jpg?alt=media&amp;token=8f35f0f7-87de-4cc5-9037-4053e2b750c0'
-    },
-    {
-        name: 'Dr. Rajani Sinha',
-        specialty: 'General Physician',
-        experience: 15,
-        consultationFees: '1000',
-        distance: 0.5,
-        rating: '96%',
-        ratingsCount: 105,
-        image: 'https://firebasestorage.googleapis.com/v0/b/doctors-list-a7aba.appspot.com/o/images%2Findian_woman_middle_aged_docto%20(8).jpeg?alt=media&amp;token=127683c7-e706-463d-bba6-da350f9c9ea6'
-    }
-];
+function InputField({text,onChange,value}) {
+    console.log(value)
+    return (
+        <div className="w-72 p-2">
+            <div className="relative w-full min-w-[200px] h-10">
+                <input className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900" placeholder=" " value={value} onChange={(e) => onChange(e.target.value)}/>
+                <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">{text}</label>
+            </div>
+        </div>
+    )
+}
+
+function DoctorCard({ doctor, id, setCurrDoctor, doctors, setIsEdit }) {
+    return (
+        <div className="card">
+            <div className="image-container">
+                <img className="image" src={doctor.image} alt={doctor.name} width="120" height="120" loading="lazy" />
+            </div>
+            <div className="info-container">
+                <h3>{doctor.name}</h3>
+                <span className="biggerSpan">{doctor.specialty}</span><br />
+                <span>{doctor.experience} years of experience</span><br />
+                <span> ₹{doctor.consultationFees} Consultation fees</span><br />
+                <span>{doctor.distance} kms from me</span><br />
+                <div className="rating-widget">
+                    <div className="rating">{doctor.rating}</div>
+                    <span className="biggerSpan">{doctor.ratingsCount} ratings provided</span>
+                </div>
+            </div>
+            <div className="button-container flex flex-col">
+                <button className="button-4">Book a visit now</button>
+                <br/>
+                <button 
+                    className="button-4"
+                    onClick={() => {
+                        setCurrDoctor(prev => doctors[id])
+                        setIsEdit(prev => false)
+                    }}
+                >
+                    Edit
+                </button>
+                
+            </div>
+        </div>
+    );
+}
 
 const Question = () => {
-    // const doctorsRef = doc(db, 'questions', '1');
-    // const saveDoctors = async () => {
-    //     try {
-    //         await setDoc(doctorsRef, { doctors });
-    //         console.log("Successfully saved doctors list.");
-    //     } catch (error) {
-    //         console.error("Error saving doctors list:", error);
-    //     }
-    // };
-
-    // saveDoctors();
-
 
     const handleClick = async () => {
         try {
@@ -89,11 +61,61 @@ const Question = () => {
             console.error('Error calling function:', error);
         }
     };
-    
+
+    const [doctors,setDoctors] = useState([])
+    const [isEdit,setIsEdit] = useState(false)
+    const [currDoctor,setCurrDoctor] = useState({
+        name: '',
+        experience: '',
+        consultationFees: '',
+        distance: '',
+        rating: '',
+        ratingsCount: ''
+    }); 
+
     return (
-        <div>
-            <button onClick={handleClick}>Call Cloud Function</button>
-        </div>
+        <>
+            <div className='flex flex-col items-center'>
+                <br/>
+                <div className='flex'>
+                    { doctors[0]? <DoctorCard id={0} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[0]}/> : "" }
+                    { doctors[1]? <DoctorCard id={1} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[1]}/> : "" }
+                </div>
+                <div className='flex'>
+                    { doctors[2]? <DoctorCard id={2} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[2]}/> : "" }
+                    { doctors[3]? <DoctorCard id={3} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[3]}/> : "" }
+                </div>
+                <div className='flex'>
+                    { doctors[4]? <DoctorCard id={4} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[4]}/> : "" }
+                    { doctors[5]? <DoctorCard id={5} setIsEdit={setIsEdit} setCurrDoctor={setCurrDoctor} doctors={doctors} doctor={doctors[5]}/> : "" }
+                </div>
+            </div>
+            <div className='flex flex-col items-center'>
+                <InputField value={currDoctor.name} text={"Doctor's Name"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, name : text })} ) }}/>
+                <InputField value={currDoctor.experience} text={"Years of Experience"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, experience : text })} ) }}/>
+                <InputField value={currDoctor.consultationFees} text={"Consultation fees"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, consultationFees : text })} ) }}/>
+                <InputField value={currDoctor.distance} text={"Distance"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, distance : text })} ) }}/>
+                <InputField value={currDoctor.rating} text={"Rating"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, rating : text })} ) }}/>
+                <InputField value={currDoctor.ratingsCount} text={"No. of ratings"} onChange={ (text) => { setCurrDoctor( prev => {return({ ...prev, ratingsCount : text })} ) }}/>
+                <button 
+                    onClick={() => { 
+                        setDoctors( prev => [...prev,currDoctor] )
+                        setCurrDoctor({
+                            name: '',
+                            experience: '',
+                            consultationFees: '',
+                            distance: '',
+                            rating: '',
+                            ratingsCount: ''
+                        }); 
+                    }}
+                    type="button" 
+                    className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >
+                    Purple to Blue
+                </button>
+            </div>
+        </>
     );
 }
 
