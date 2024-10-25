ad60a1aed77492a9089f3aa3110944e6779864e8
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-10-25 12:57:15 +0530

    test

--- website/components/utils/convertDriveLinkToImageURL.js
+++ website/components/utils/convertDriveLinkToImageURL.js
@@ -1,33 +1,21 @@
 const readline = require('readline');
 
-// Function to convert the Google Drive link to a direct image URL
-function convertDriveLinkToImageURL(driveLink) {
-    const fileId = driveLink.match(/[-\w]{25,}/);
-
-    if (fileId) {
-        const imageUrl = `https://drive.google.com/uc?export=view&id=${fileId[0]}`;
-        return imageUrl;
+function convertGithubUrlToRaw(githubUrl) {
+    if (githubUrl.includes('github.com') && githubUrl.includes('/blob/')) {
+        let rawUrl = githubUrl.replace('github.com', 'raw.githubusercontent.com');
+        rawUrl = rawUrl.replace('/blob/', '/');
+        return rawUrl;
     } else {
-        return null;
+        return "Invalid GitHub URL. Please provide a valid URL for an image.";
     }
 }
 
-// Set up readline interface for user input
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// Ask the user for the Google Drive link
-rl.question("Enter Google Drive link: ", (driveLink) => {
-    const imageUrl = convertDriveLinkToImageURL(driveLink);
-
-    if (imageUrl) {
-        console.log("Direct Image URL:", imageUrl);
-    } else {
-        console.log("Invalid Google Drive link provided.");
-    }
-
-    // Close the readline interface
-    rl.close();
+rl.question("Enter the GitHub image URL: ", function(githubImageUrl) {
+    const rawImageUrl = convertGithubUrlToRaw(githubImageUrl);
+    console.log("Raw Image URL:", rawImageUrl);
 });
